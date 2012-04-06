d17ce45f5c91191c6afc354dc7914535705b7118
Mike Ivanov <mivanov@gmail.com>
2012-04-05 17:01:54 -0700

    Downgrade psycopg2 on Ubuntu 11.10 before testing

--- fabfile.py
+++ fabfile.py
@@ -14,13 +14,18 @@ prompts = []
 prompts += fexpect.expect('3. Paste the API key below:', 'e886fd33ac5743868c31dfebecdb129b')
 prompts += fexpect.expect('Would you like to create one now?', 'no')
 
-def test_install(os):
-    with temporary_ec2_instance(ami_id=images[os]):
+def test_install(image_name):
+    with temporary_ec2_instance(ami_id=images[image_name]):
         sudo('apt-get -y install python-software-properties')
         sudo('yes | apt-add-repository ppa:localwikidev/testing')
         sudo('apt-get update')
         with fexpect.expecting(prompts):
             fexpect.sudo('apt-get -y --force-yes install localwiki')
+        # On Ubuntu 11.10, downgrade to psycopg2 v2.4.1 or else tests can't run
+        # See https://code.djangoproject.com/ticket/16250
+        if image_name == 'ubuntu11.10':
+            sudo('apt-get -y install python-dev libpq-dev')
+            sudo('pip install psycopg2==2.4.1')
         # Give localwiki db user permission to create a database, for tests
         sudo('psql -c "ALTER ROLE localwiki WITH CREATEDB;"', user='postgres')
         sudo('localwiki-manage test pages maps tags versioning diff ckeditor redirects users')
