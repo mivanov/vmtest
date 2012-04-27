4a055274b8a8b969e4f5ff747f9e3d3b6cfd5e02
Mike Ivanov <mivanov@gmail.com>
2012-04-27 09:19:10 -0700

    Add Ubuntu 12.04

--- fabfile.py
+++ fabfile.py
@@ -7,7 +7,8 @@ import fexpect
 images = { 'ubuntu10.04': 'ami-8cb33ebc',
            'ubuntu10.10': 'ami-04f97434',
            'ubuntu11.04': 'ami-18f97428',
-           'ubuntu11.10': 'ami-20f97410'
+           'ubuntu11.10': 'ami-20f97410',
+           'ubuntu12.04': 'ami-3e800c0e'
           }
 
 prompts = []
@@ -23,7 +24,7 @@ def test_install(image_name):
             fexpect.sudo('apt-get -y --force-yes install localwiki')
         # On Ubuntu 11.10, downgrade to psycopg2 v2.4.1 or else tests can't run
         # See https://code.djangoproject.com/ticket/16250
-        if image_name == 'ubuntu11.10':
+        if image_name in ['ubuntu11.10', 'ubuntu12.04']:
             sudo('apt-get -y install python-dev libpq-dev')
             sudo('pip install psycopg2==2.4.1')
         # Give localwiki db user permission to create a database, for tests
